f3afb47db8a307bf1aa86e3051575acbc2b718c3
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-06-24 15:32:56 +0700

    Update config.py

--- Himawari/config.py
+++ Himawari/config.py
@@ -52,7 +52,7 @@ class Config(object):
     API_HASH = ""  # api hash from my.telegram.org
     # mongo database link (necessary)
     MONGO_DB_URL = ""
-    DB_URL = "postgresql://ttyzrvpi:AlRWAnTpM4vPgacujfA-s08CGb8AHrZR@mahmud.db.elephantsql.com/ttyzrvpi"  # postgres sql database link
+    DB_URL = "postgresql://doadmin:AVNS_lfJ2vYAiAU9XDtdqWMb@db-postgresql-nyc1-85006-do-user-14162283-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"  # postgres sql database link
     # redis database url from redislabs.com
     REDIS_URL = ""
     TOKEN = ""  # bot token from @BotFather
